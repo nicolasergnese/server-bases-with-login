04e86ca1be3312ffab414b2a1c13b4e4eeca2e1a
Nicola Sergnese <nicola.sergnese@eng.it>
2023-05-25 11:02:51 +0200

    add response

--- routes/auth.js
+++ routes/auth.js
@@ -19,7 +19,7 @@ router.post("/login",async(req,res)=>{
         if (!validPassword)
             return res.status(401).send({message:"Invalid Email or Password"});
         const token = user.generateAuthToken();
-        res.status(200).send({data:token,message:"Logged in successfully"});
+        res.status(200).send({token:token,message:"Logged in successfully"});
 
     } catch (error) {
         res.status(500).send({message:"Intern Server Error"});
