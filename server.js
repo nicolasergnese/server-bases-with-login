25e408df424a0569ffa7d27492c06f4d8cd9dbd0
giurbinati <gianmarco.urbinati@eng.it>
2023-05-30 09:31:57 +0200

    fix data chart

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const db = require("./db");
 const userRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
 
-const chartRoutes = require('./routes/chart');//collegamento api chart
+const chartRoutes = require('./routes/chart');//collegamento api chart Value
+const chartRoutesDateTime = require('./routes/chartDateTime')//collegamento api chart DateTime
 
-const offersRoutes = require('./routes/offers'); //collegamento api offers
+//const offersRoutes = require('./routes/offers'); //collegamento api offers
 
 
 //const sqlRoutes = require('./routes/sql')
@@ -17,7 +18,7 @@ const offersRoutes = require('./routes/offers'); //collegamento api offers
 
 
 //database connection
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+//db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 
 //middlewares
@@ -33,6 +34,7 @@ app.use("/api/auth", authRoutes);
 
 app.use("/api/chart", chartRoutes);//collegamento api chart
 //app.use("/api/offers", offersRoutes) //collegamento api per tabella offers
+app.use("/api/chartDateTime", chartRoutesDateTime)
 
 
 const port = process.env.PORT || 8080;
@@ -56,88 +58,82 @@ connectionMQTT.on('message', function (topic, message) { //riceve messaggi da mq
   connectionMQTT.end() //chiusura connessione
 })    */
 
-
-
-
 /* 
-  const sql = require('mssql'); //qui invece con il metodo mssql mi da credenziali errate
+const sql = require('mssql'); //qui invece con il metodo mssql mi da credenziali errate
 const config = {
     user: 'Read_Only_ENG_MATRYCS',
     password: 'MATRYCS2021!',
     server: '172.16.1.136', // You can use 'localhost\\instance' to connect to named instance, telnet 172.16.1.136 1433
-    database: 'History',
+    database: 'Runtime',
     options: {
       encrypt: false // Use this if you're connecting to Azure SQL Database with true
     }
 };
-async function connect() {
-    try {
-        await sql.connect(config);
-        console.log('Connected to MSSQL');
-    } catch (err) {
-        console.error(err);
-    }
-}
-connect();    */
 
-
-/*const sql = require('mssql') //questa presa direttamnte da npm,va direttamnetein catch non so il perchè
-async function connect() {
-    try {
-        // make sure that any items are correctly URL encoded in the connection string
-        await sql.connect('Server=172.16.1.136,1433;Database=Asm0103;User Id=Read_Only_ENG_MATRYCS;Password=MATRYCS2021!;Encrypt=false')
-        //const result = await sql.query`select * from mytable where id = ${value}`
-        console.log('Connected to MSSQL')
-    } catch (err) {
-      console.log(' NOT connected to MSSQL')
-        // ... error checks
-    }
+async function executeQuery() {
+  try {
+    await sql.connect(config);
+    const result = await sql.query(`
+      SELECT DateTime, Value
+      FROM dbo.History
+      WHERE tagName = 'min_DB_ENM_001638.Cv'
+      AND DateTime between '2022-07-01 00:00:00'
+      AND  '2022-07-01 01:00:00'
+    `);
+    
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    sql.close();
+  }
 }
-connect(); */
+
+executeQuery(); */
+
+/*
+async function connect() {
+  try {
+      await sql.connect(config);
+      console.log('Connected to MSSQL');
+  } catch (err) {
+      console.error(err);
+      console.log(err)
+  }
+}
+  
+ async function executeQuery() {
+  try {
+    // Create a new connection pool
+    connect();
+    const result = await sql.query`SELECT DateTime,Value FROM dbo.History WHERE  tagName = 'min_DB_ENM_001638.Cv' AND dateTime >= '2022-07-01 00:00:00' AND dateTime < '2022-08-01 00:00:00'`;
+    //console.log(result.recordset);
+    /* const query= "SELECT * FROM dbo.history";
+    // Execute the query asynchronously
+    const result = await sql.query(query);
+    // Access the fetched records
+    console.log(result.recordset);
+    // Close the connection pool
+    await sql.close(); 
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+// Call the executeQuery function
+executeQuery(); */
 
 
 
 /* const sql = require("msnodesqlv8") //qui mi da credenziali errate, anche se la query è errata
 const connectionString = "server=172.16.1.136;Database=ASM0103;Uid=Read_Only_ENG_MATRYCS;Pwd=MATRYCS2021!;Trusted_Connection=Yes;Driver={SQL Server}";
 const query= "SELECT * FROM Runtime.dbo.IOSServerType";
-//const query= "SELECT DateTime, Value from Runtime.db0.History Where (Tagname='min_DB_ENM_001638.Cv') and (DateTime) >= '2022-07-01 00:00:00' and (DateTime) <'2022-08-01 00:00:00'";
+//const query= "SELECT DateTime, Value from Runtime.dbo.History Where (Tagname='min_DB_ENM_001638.Cv') and (DateTime) >= '2022-07-01 00:00:00' and (DateTime) <'2022-08-01 00:00:00'";
 sql.query(connectionString, query, (err, rows) => {
   console.log(rows);
   console.log(err);
 })   */
 
 
-/* const sql = require('msnodesqlv8');
-var dbConfig = {
-    connectionTimeout : 30000,
-    database: "<Asm0103>",
-    server: "<172.16.1.136>",
-    options: {
-        trustedConnection: true,
-    }
-};
-var dbConfig2 = {
-    connectionTimeout : 30000,
-    connectionString: 'Driver={SQL Server Native Client 11.0};Server=<172.16.1.136>;Database=<Asm0103>;Trusted_Connection=yes;',
-    options: {
-        
-    }
-};
-async function connect() {
-    try {
-        console.log("start");
-        await sql.connect(dbConfig);  // both format of dbConfig or dbConfig2 will work
-        const result = await sql.query(`select SYSDATETIME() as dt `);
-        console.dir(result.recordset);
-        console.log("end");
-    } catch (err) {
-        // 
-        console.log(err);
-    } finally {
-        //
-    }
-};
-connect(); */
 
 
 /* 
@@ -190,112 +186,19 @@ sql.open(connectionString, (err, conn) => {
     });
   }
 });  */
+/* app.post('/api/myEndpoint', (req, res) => {
+  try {
+    const value = req.body.value;
 
+    // Fai qualcosa con la variabile ricevuta dal front-end
 
-
-/*
- const mysql = require('mysql'); //con mysql mi da questo errore: connect ECONNREFUSED 172.16.1.136:3306
-
-const connection = mysql.createConnection({ //connessione dati sql
-   host: '172.16.1.136',
-   user: 'Read_Only_ENG_MATRYCS',
-   password: 'MATRYCS2021!',
-   database: 'Asm0103',
-});
-connection.connect((err) => {
- if (err) {
-   console.error('Error connecting to MySQL database: ' + err.stack);
-   return;
- }
- console.log('Connected to MySQL database with id ' + connection.threadId);
-});
-connection.connect(function(err) {
-   if (err) throw err;
-   console.log("Connected!");
-   //Insert a record in the "customers" table:
-   //var sql = "INSERT INTO impiegati (impiegato_nome, azienda) VALUES ('Gianmarco', 'Eng')"; //query per inserire dato nel database sql
-   //connection.query(sql, function (err, result) { //funzione che invia la query
-     if (err) throw err; //se c'è errore
-     console.log("1 record inserted");
-   });
- //});    */
-
-/* var Connection = require('tedious').Connection;  //questo è un ltro metodo con tedious, qui mi da errore di Unexpected token '??=', ma non so dove
-
-var config = {
-  server: "172.16.1.136", // or "localhost"
-  options: {},
-  authentication: {
-    type: "default",
-    options: {  
-      userName: "Read_Only_ENG_MATRYCS",
-      password: "MATRYCS2021!",
-  }},
-  options:{
-    encrypt: true,
-    database: 'ASM0103'
+    // Invia una risposta al front-end
+    res.send('Dati ricevuti con successo!');
+    console.log(value)
+  } catch (error) {
+    // Gestisci l'errore di elaborazione
+    res.status(500).send('Si è verificato un errore durante l\'elaborazione dei dati.');
   }
-};
-
-var connection = new Connection(config);
-
-// Setup event handler when the connection is established. 
-connection.on('connect', function(err) {
-    console.log('prova')
-});
-
-// Initialize the connection.
-connection.connect(); */
+}); */
 
 
-/* 
- const tedious = require('tedious');//questo è un ltro metodo con tedious, qui mi da errore di Unexpected token '??=', ma non so dove
-
-    const config = {
-      server: '172.16.1.136',
-      authentication: {
-        type: 'default',
-        options: {
-          userName: 'Read_Only_ENG',
-          password: 'MATRYCS2021!',
-        },
-      },
-      options: {
-        database: 'Asm0103',
-        encrypt: false, // Use SSL encryption (optional)
-      },
-    };
-    
-    const connection = new tedious.Connection(config);
-
-    connection.on('connect', (err) => {
-      if (err) {
-        console.error('Errore di connessione al database: ' + err.message);
-      } else {
-        console.log('Connesso al database SQL Server');
-        // Puoi eseguire query o altre operazioni qui
-      }
-    });
-  
-
-// Avvia la connessione al database
-connection.connect(); */
-
-/*     const { Pool } = require('pg');  //connessione con postgres, mi da questo errore:Error connecting to PostgreSQL database: Error: Connection terminated unexpectedly
-    const pool = new Pool({
-      user: 'Read_Only_ENG_MATRYCS',
-      host: '172.16.1.136',
-      database: 'Asm0103',
-      password: 'MATRYCS2021!',
-      port: 1433,
-    });
-    pool.connect((err, client, release) => {
-      if (err) {
-        console.error('Error connecting to PostgreSQL database: ' + err.stack);
-        return;
-      }
-    
-      console.log('Connected to PostgreSQL database with process id ' + client.processID);
-    
-      release();
-    }); */
