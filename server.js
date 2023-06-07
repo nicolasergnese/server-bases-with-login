2eeb78a8a2c4e7b84bc40c224e4e5d041df44c48
giurbinati <gianmarco.urbinati@eng.it>
2023-06-07 15:03:37 +0200

    Revert "update server with query"
    This reverts commit ea9467039f2b8ad39d617a04b261054db63579c4.

--- server.js
+++ server.js
@@ -7,10 +7,18 @@ const db = require("./db");
 const userRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
 
-//const chartRoutes = require('./routes/chart');//collegamento api chart Value
+const chartRoutes = require('./routes/chart');//collegamento api chart Value
+const chartRoutesDateTime = require('./routes/chartDateTime')//collegamento api chart DateTime
+
+//const offersRoutes = require('./routes/offers'); //collegamento api offers
+
+
+//const sqlRoutes = require('./routes/sql')
+//const dbSql = require('./dbSql')
+
 
 //database connection
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+//db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 
 //middlewares
@@ -24,90 +32,173 @@ app.use(cors());
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
- //app.use("/api/chart", chartRoutes);//collegamento api chart, non lo riesco a collegare
-
- 
-let powerValue = ''; // Dichiarazione globale di powerValue
-let datestart = ''; // Dichiarazione globale di datestart
-let dateend = ''; // Dichiarazione globale di dateend
-
-app.post('/api/power', async (req, res) => { //collegamento per prendere i dati di power dal front-end per utilizzarle nella query
-  try {
-    powerValue = req.body.power; // Update the variable name to "power" instead of "meter"
-  
-    // Do whatever you want with the power value
-    console.log(`Power value: ${powerValue}`);
-  
-    // Send a response to the client
-    res.json({ message: 'Power value received' });
-  } catch (error) {
-    // Handle any errors that may occur
-    console.error(error);
-    res.status(500).json({ message: 'An error occurred' });
-  }
-});
- 
-// Gestione della richiesta POST per la data
-app.post('/api/datetimestart', (req, res) => { //collegamento per prendere le date inizali dai DateAndTimestart  dal front-end per utilizzarle nella query
-  datestart = req.body.datestart;
-  console.log(`date start: ${datestart}`);
-  // Esegui le operazioni necessarie con la data (es. salvataggio nel database)
-  // Invia una risposta di conferma al front end
-  res.json({ message: 'Data received successfully' });
-});
-
-
-// Gestione della richiesta POST per la data
-app.post('/api/datetimeend', (req, res) => { //collegamento per prendere le date finali dai DateAndTimeend dal front-end per utilizzarle nella query
-  dateend  = req.body.dateend;
-  // Esegui le operazioni necessarie con la data (es. salvataggio nel database)
-  // Invia una risposta di conferma al front end
-  console.log(`date end: ${dateend}`);
-  res.json({ message: 'Data received successfully' });
-});
-
-
-const sql = require('mssql'); //libreria mssql
-
-const config = { //credenziali di accesso di mssql
-  user: 'Read_Only_ENG_MATRYCS',
-  password: 'MATRYCS2021!',
-  server: '172.16.1.136',
-  database: 'Runtime',
-  options: {
-    encrypt: false
-  }
-};
-
-async function executeSQLQuery(query) { //funzione generica per eseguire una query per risolvere il problema di sincronizzazione dei dati
-  await sql.connect(config);
-  const result = await sql.query(query);
-  sql.close();
-  return result.recordset;
-}
-
-app.get("/api/chartDateTime", async (req, res) => { //esecuzione della query con i dati presi dal front-end
-  try {
-    console.log(`date end: ${dateend}`);
-    console.log(datestart);
-    console.log(dateend); 
-    const result = await executeSQLQuery(`
-      SELECT DateTime, Value
-      FROM dbo.History
-      WHERE tagName = '${powerValue}'
-      AND DateTime between '${datestart}'
-      AND  '${dateend}'
-    `);  
-    sql.close();
-    console.log('finito');
-    return res.status(201).send(result);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send('Internal Server Error');
-  }
-});
+app.use("/api/chart", chartRoutes);//collegamento api chart
+//app.use("/api/offers", offersRoutes) //collegamento api per tabella offers
+app.use("/api/chartDateTime", chartRoutesDateTime)
 
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
+/*
+const mqtt = require('mqtt') //connessione a mqtt online generico,  ancora non testato sul loro mqtt
+const connectionMQTT  = mqtt.connect('mqtt://test.mosquitto.org') //connessione a mqtt
+connectionMQTT.on('connect', function () { //funzione per connettersi
+  connectionMQTT.subscribe('presence', function (err) {
+    if (!err) {
+      connectionMQTT.publish('presence', 'Hello mqtt') //invia messaggi a mqtt
+    }
+  })
+})
+connectionMQTT.on('message', function (topic, message) { //riceve messaggi da mqtt
+  // message is Buffer
+  console.log(message.toString())
+  console.log("ciao")
+  connectionMQTT.end() //chiusura connessione
+})    */
+
+/* 
+const sql = require('mssql'); //qui invece con il metodo mssql mi da credenziali errate
+const config = {
+    user: 'Read_Only_ENG_MATRYCS',
+    password: 'MATRYCS2021!',
+    server: '172.16.1.136', // You can use 'localhost\\instance' to connect to named instance, telnet 172.16.1.136 1433
+    database: 'Runtime',
+    options: {
+      encrypt: false // Use this if you're connecting to Azure SQL Database with true
+    }
+};
+
+async function executeQuery() {
+  try {
+    await sql.connect(config);
+    const result = await sql.query(`
+      SELECT DateTime, Value
+      FROM dbo.History
+      WHERE tagName = 'min_DB_ENM_001638.Cv'
+      AND DateTime between '2022-07-01 00:00:00'
+      AND  '2022-07-01 01:00:00'
+    `);
+    
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    sql.close();
+  }
+}
+
+executeQuery(); */
+
+/*
+async function connect() {
+  try {
+      await sql.connect(config);
+      console.log('Connected to MSSQL');
+  } catch (err) {
+      console.error(err);
+      console.log(err)
+  }
+}
+  
+ async function executeQuery() {
+  try {
+    // Create a new connection pool
+    connect();
+    const result = await sql.query`SELECT DateTime,Value FROM dbo.History WHERE  tagName = 'min_DB_ENM_001638.Cv' AND dateTime >= '2022-07-01 00:00:00' AND dateTime < '2022-08-01 00:00:00'`;
+    //console.log(result.recordset);
+    /* const query= "SELECT * FROM dbo.history";
+    // Execute the query asynchronously
+    const result = await sql.query(query);
+    // Access the fetched records
+    console.log(result.recordset);
+    // Close the connection pool
+    await sql.close(); 
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+// Call the executeQuery function
+executeQuery(); */
+
+
+
+/* const sql = require("msnodesqlv8") //qui mi da credenziali errate, anche se la query è errata
+const connectionString = "server=172.16.1.136;Database=ASM0103;Uid=Read_Only_ENG_MATRYCS;Pwd=MATRYCS2021!;Trusted_Connection=Yes;Driver={SQL Server}";
+const query= "SELECT * FROM Runtime.dbo.IOSServerType";
+//const query= "SELECT DateTime, Value from Runtime.dbo.History Where (Tagname='min_DB_ENM_001638.Cv') and (DateTime) >= '2022-07-01 00:00:00' and (DateTime) <'2022-08-01 00:00:00'";
+sql.query(connectionString, query, (err, rows) => {
+  console.log(rows);
+  console.log(err);
+})   */
+
+
+
+
+/* 
+const sql = require('msnodesqlv8');
+const connectionString = "Driver={SQL Server Native Client 11.0};Server=172.16.1.136;Database=Asm0103;Uid=Read_Only_ENG_MATRYCS;Pwd=MATRYCS2021!;";
+sql.open(connectionString, function (err, conn) {
+  if (err) {
+    console.error("Error opening the connection:", err);
+    return;
+  }
+  console.log("Connected to SQL Server!");
+  // Perform database operations here
+  // Close the connection when finished
+  conn.close(function (err) {
+    if (err) {
+      console.error("Error closing the connection:", err);
+      return;
+    }
+    console.log("Connection closed.");
+  });
+}); */
+
+
+/* 
+const sql = require('msnodesqlv8');
+const connectionString = 'Driver={SQL Server Native Client 11.0};Server=172.16.1.136;Database=Asm0103;Uid=Read_Only_ENG_MATRYCS;Pwd=MATRYCS2021!;';
+sql.open(connectionString, (err, conn) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  } else {
+    console.log('Connection successful!');
+    
+    // Your code for executing queries or performing database operations
+    const query ="SELECT * from db_datareader";
+    conn.query(query, (err, results) => {
+      if (err) {
+        console.error('Query execution failed:', err);
+      } else {
+        console.log('Query results:', results);
+      }
+    });
+    
+    // Remember to close the connection when you're done
+    conn.close((err) => {
+      if (err) {
+        console.error('Error closing connection:', err);
+      } else {
+        console.log('Connection closed.');
+      }
+    });
+  }
+});  */
+/* app.post('/api/myEndpoint', (req, res) => {
+  try {
+    const value = req.body.value;
+
+    // Fai qualcosa con la variabile ricevuta dal front-end
+
+    // Invia una risposta al front-end
+    res.send('Dati ricevuti con successo!');
+    console.log(value)
+  } catch (error) {
+    // Gestisci l'errore di elaborazione
+    res.status(500).send('Si è verificato un errore durante l\'elaborazione dei dati.');
+  }
+}); */
+
 
