adf2faac958439e56e15a47d4725fe601fd9bebd
giurbinati <gianmarco.urbinati@eng.it>
2023-09-28 10:30:55 +0200

    ficx problem

--- routes/optimized.js
+++ routes/optimized.js
@@ -16,12 +16,11 @@ router.post("/api/numberStep", async (req, res) => {
 });
 
 const apiUrl = 'http://rl-smart-optimizer.jmira.kserve.kf.iot-ngin.onelab.eu:80/v1/models/rl-optimizer:predict';
-
-
 router.get("/api/chartDateTimeHome", async (req, res) => {
     try {
         const requestData = {
             number_steps: numberStep,
+            //state_index: 0
             state_index: -numberStep
         };
         const requestOptions = {
@@ -31,7 +30,7 @@ router.get("/api/chartDateTimeHome", async (req, res) => {
                 'Accept-Encoding': 'gzip, deflate',
                 'Connection': 'keep-alive',
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Cookie': 'authservice_session=MTY5NTY1NDgxOHxOd3dBTkV4WVVFRkROVkZaTjFaUFdETkRXbHBVUjA4MU5GTklNak5TUWxkVVJrYzFSRmhPU1RRME0xSkpUak5ETlU0M1J6VlhNMUU9fOwwJrPQboAO9bkthLXzLCe7kvIGhqDm7MOebi8v74Yp',
+                'Cookie': 'authservice_session=MTY5NTc0MzI2MXxOd3dBTkRkV05VSmFRa05EVWtGSU5WcFZXRGN6VDBOSVJFRXpORkEwTkU5RldWbElSREpKUWxOR1VVazNRakpaVmxkSU56VlZTVkU9fEe6hTw-VznCFI_FPtWwM96qbvQd1xKM43Pcb9bKaXHV',
                 'User-Agent': 'python-requests/2.22.0'
             },
             body: JSON.stringify(requestData)
@@ -42,7 +41,39 @@ router.get("/api/chartDateTimeHome", async (req, res) => {
         const data = await response.json();
         console.log('Response data:', data);
         const kpis = data.state_requested.kpis
+        console.log(kpis)
         return res.status(201).send(kpis);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+router.get("/api/chartDateTimeHome2", async (req, res) => {
+    try {
+        const requestData2 = {
+            number_steps: 0,
+            state_index: -1
+        };
+        const requestOptions2 = {
+            method: 'POST',
+            headers: {
+                'Accept': '*/*',
+                'Accept-Encoding': 'gzip, deflate',
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cookie': 'authservice_session=MTY5NTc0MzI2MXxOd3dBTkRkV05VSmFRa05EVWtGSU5WcFZXRGN6VDBOSVJFRXpORkEwTkU5RldWbElSREpKUWxOR1VVazNRakpaVmxkSU56VlZTVkU9fEe6hTw-VznCFI_FPtWwM96qbvQd1xKM43Pcb9bKaXHV',
+                'User-Agent': 'python-requests/2.22.0'
+            },
+            body: JSON.stringify(requestData2)
+        };
+        console.log(requestData2.number_steps);
+        console.log(requestData2.state_index);
+        const response = await fetch(apiUrl, requestOptions2);
+        const data = await response.json();
+        console.log('Response data 2:', data);
+        const kpis2 = data.state_requested.kpis
+        console.log(kpis2)
+        return res.status(201).send(kpis2);
     } catch (error) {
         console.error(error);
     }
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const asmhqRoutes = require('./routes/asmhq') //utilizzo i servizi della pagina 
 const dsoRoutes = require('./routes/dso') //utilizzo i servizi della pagina DSO, decreto il vincitore
 const forecasted = require('./routes/forecasted') //utilizzo i servizi della pagina FORECASTED, cookies
 const energiot = require('./routes/energiot') //utilizzo i servizi della pagina ENERGIOT, cookies
-const home = require('./routes/home') //utilizzo i servizi della pagina ATOS, cookies
+const optimized = require('./routes/optimized') //utilizzo i servizi della pagina ATOS, cookies
 
 //database connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
@@ -34,62 +34,10 @@ app.use(asmhqRoutes); //utilizzo i servizi per la pagina ASMHQ, mssql
 app.use(dsoRoutes); //utilizzo i servizi per la pagina DSO, vincitore di una request
 app.use(forecasted); //utilizzo i servizi della pagina FORECASTED, cookies
 app.use(energiot); //utilizzo i servizi della pagina ENERGIOT, cookies
-app.use(home); //utilizzo i servizi della pagina ATOS, cookies
-
-/* 
-//const express = require('express');
-const router = express.Router();
-const fetch = require('node-fetch');
-
-const axios = require('axios');
-
-
-const apiUrl = 'http://rl-smart-optimizer.jmira.kserve.kf.iot-ngin.onelab.eu:80/v1/models/rl-optimizer:predict';
-const requestData = {
-  number_steps: 4,
-  state_index: -1
-};
+app.use(optimized); //utilizzo i servizi della pagina ATOS, cookies
 
 
 
-router.get("/api/chartDateTimeHome", async (req, res) => {
-    try {
-        fetch(apiUrl, requestOptions)
-        .then(response => response.json())
-        .then(data => console.log('Response data:', data))
-        .catch(error => console.error('Error:', error));   
-    } catch (error) {
-        console.error(error);
-    }
-}); */
-
-
-/* const ciao = async (req, res) => {
-  try {
-      fetch(apiUrl, requestOptions)
-      .then(response => response.json())
-      .then(data => console.log('Response data:', data))
-      .catch(error => console.error('Error:', error));   
-  } catch (error) {
-      console.error(error);
-  }
-};
-ciao();
- */
-
-/* const ciao = async () => {
-  try {
-    const response = await fetch(apiUrl, requestOptions);
-    const data = await response.json();
-    console.log('Response data:', data);
-    const kpis = data.state_requested.kpis
-    console.log(kpis)
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
-
-ciao(); */
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
