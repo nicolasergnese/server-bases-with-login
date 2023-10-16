8a568c51896b9bb57a098f47321304b3c881ea83
giurbinati <gianmarco.urbinati@eng.it>
2023-10-16 10:55:33 +0200

    fix problem forecasted

--- routes/forecasted.js
+++ routes/forecasted.js
@@ -46,13 +46,13 @@ router.get("/api/chartDateTimeForecasted", async (req, res) => {
     try {
         if (sensorIdValue === 'W4') {
         //const authservice_session = 'MTY5MzIxODU1OHxOd3dBTkVSU1ZFMU1ORXBOTjA5TE5ESklRMHBPVTBKSFIwazNSRVZIVUZwR1RreGFXa2hQU2tGR1JFRk5VMFJKUVRSUVJrcERNMEU9fAnVRXZz540UQ2UJpEIkwn0Pi-VH5QAfkbSPwgzIdzqd';
-        const url = 'http://power-consumption-uc-ten.jmira.kserve.kf.iot-ngin.onelab.eu/v1/models/power-consumption-uc-ten:predict';
+        const url = 'http://power-w4-uc-ten.jmira.kserve.kf.iot-ngin.onelab.eu/v1/models/power-w4-uc-ten:predict';
         const headers = {
             'Accept': '*/*',
             'Accept-Encoding': 'gzip, deflate',
             'Connection': 'keep-alive',
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': 'authservice_session=MTY5Njg1OTc4NXxOd3dBTkVOV00wSkpOa1pITWtkVldrUlZNbE5TVVVaWVUxaE1XVU5FUTB0R1RUSlNSVWhYVUVwWlVrOUxWVFJPVUZWSVYwaE9Ra0U9fIVT2l2EON82nZOp6uCKCTJjTadbzZm-4TeV-us12dwY',
+            'Cookie': 'authservice_session=MTY5NzQ0NTE4N3xOd3dBTkZoUlJsUkJVbFkyU0Vnek5VRTFXVUZPUWxOTFdUSkJWbFpJU0VGT05rcFlORVZWUzBNM1UweFlURmhWVlRJMVFUZFVRMUU9fKUg-benrb0DZhKfDwNHpzXkJB6PH7Lid7OTVBgQJVQk',
             'User-Agent': 'python-requests/2.22.0'
         };
         const payload = {};
@@ -67,13 +67,13 @@ router.get("/api/chartDateTimeForecasted", async (req, res) => {
         return res.status(201).send(data.predictions.value);
     } else if(sensorIdValue === 'W6') {
         //const authservice_session = 'MTY5MzIxODU1OHxOd3dBTkVSU1ZFMU1ORXBOTjA5TE5ESklRMHBPVTBKSFIwazNSRVZIVUZwR1RreGFXa2hQU2tGR1JFRk5VMFJKUVRSUVJrcERNMEU9fAnVRXZz540UQ2UJpEIkwn0Pi-VH5QAfkbSPwgzIdzqd';
-        const url = 'http://power-generation-uc-ten.jmira.kserve.kf.iot-ngin.onelab.eu/v1/models/power-generation-uc-ten:predict';
+        const url = 'http://power-w6-uc-ten.jmira.kserve.kf.iot-ngin.onelab.eu/v1/models/power-w6-uc-ten:predict';
         const headers = {
             'Accept': '*/*',
             'Accept-Encoding': 'gzip, deflate',
             'Connection': 'keep-alive',
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': 'authservice_session=MTY5NTg5MDE2M3xOd3dBTkVOQlNVTXpSVFEzUmxSSFVVUlJTelZTV0VJelMwMHpOa1F5UTFJelEwUldOMVZLTWtvMVMwUkNRa3d6VVVOTVEweGFVMUU9fEAar8714IY-zaaiUpRODIkgbuYMAy2mGq5txBgSIL2J',
+            'Cookie': 'authservice_session=MTY5NzQ0NTE4N3xOd3dBTkZoUlJsUkJVbFkyU0Vnek5VRTFXVUZPUWxOTFdUSkJWbFpJU0VGT05rcFlORVZWUzBNM1UweFlURmhWVlRJMVFUZFVRMUU9fKUg-benrb0DZhKfDwNHpzXkJB6PH7Lid7OTVBgQJVQk',
             'User-Agent': 'python-requests/2.22.0'
         };
         const payload = {};
